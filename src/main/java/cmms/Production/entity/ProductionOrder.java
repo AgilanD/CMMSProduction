b219c7eb469a7c92a518b82390a91849e39e8a37
AgilanD <agilan.d@mitrahsoft.in>
2026-06-24 18:44:30 +0530

    24-06-2026 Changes

--- src/main/java/cmms/Production/entity/ProductionOrder.java
+++ src/main/java/cmms/Production/entity/ProductionOrder.java
@@ -6,8 +6,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Generated;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "production_orders")
@@ -51,6 +56,37 @@ public class ProductionOrder {
     @Temporal(TemporalType.DATE)
     @Column(name = "actual_end_date")
     private LocalDate actualEndDate;
+
+
+
+    @CreatedDate
+    @Builder.Default
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT '2026-06-23 19:54:30'")
+    private LocalDateTime createdAt = LocalDateTime.parse("2026-06-23T19:54:30");
+
+    @CreatedBy
+    @Builder.Default
+    @Column(name = "created_by", nullable = false, columnDefinition = "BIGINT DEFAULT 1")
+    private Long createdBy = 1L;
+
+    @LastModifiedDate
+    @Builder.Default
+    @Column(name = "last_modified_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT '2026-06-23 19:54:30'")
+    private LocalDateTime lastModifiedAt = LocalDateTime.parse("2026-06-23T19:54:30");
+
+    @LastModifiedBy
+    @Builder.Default
+    @Column(name = "last_modified_by", nullable = false, columnDefinition = "BIGINT DEFAULT 1")
+    private Long lastModifiedBy = 1L;
+
+
+
+
+
+
+
+
+
 
 
     public enum OrderStatus {
